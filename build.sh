ce4b4f5bd9d2e4a2649101234ea7ca08b2afae66
davidprasanna <rdavidprasanna97@gamil.com>
2023-01-17 16:44:53 +0530

    build.sh is modified

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ if [[ ! -d $STORAGE_DIR/chrome ]]; then
   cd $HOME/project/src # Make sure we return to where we were
 else
   echo "...Using Chrome from cache"
-
+fi
 # be sure to add Chromes location to the PATH as part of your Start Command
 export PATH="${PATH}:/opt/render/project/.render/chrome/opt/google/chrome/"
 
